d3f1c6e6b19e87c059227a6436bb88f016418319
AlanNogueira <65971896+AlanNogueira@users.noreply.github.com>
2023-03-31 23:20:05 -0300

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 import './assets/css/global.css';
 import './assets/css/pokedex.css';
+
+function pokemonConvertToLi(pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+
+      <div class="detail">
+       <ol class="types">
+         <li class="type">Grass</li>
+         <li class="type">Poison</li>
+       </ol>
+       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         alt="${pokemon.name}"
+       />
+      </div>
+    </li>
+  `;
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemons) => {
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    pokemonList.innerHTML += pokemonConvertToLi(pokemon);
+  }
+});
